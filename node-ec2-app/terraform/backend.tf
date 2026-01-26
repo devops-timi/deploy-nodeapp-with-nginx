84d019264171d25e5308dc145e410ec326321177
devops-timi <tobalereko@gmail.com>
2026-01-26 04:35:15 +0100

    backend.tf

--- node-ec2-app/terraform/backend.tf
+++ node-ec2-app/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-      bucket         = "terraform-state-nodeappec2"
+      bucket         = "terraform-state-nodeappnginx"
       key            = "deploy-nodeapp-with-nginx/node-ec2-app/terraform.tfstate"
       region         = "us-east-1"
       dynamodb_table = "terraform-state-lock"
