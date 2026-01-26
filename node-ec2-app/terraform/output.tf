57e3aa2d501f57f91e3362342cec52022df18b3e
devops-timi <tobalereko@gmail.com>
2026-01-26 01:33:08 +0100

    output.tf

---
+++ node-ec2-app/terraform/output.tf
@@ -0,0 +1,23 @@
+output "instance_public_ip" {
+  value = aws_instance.node_app_ec2.public_ip
+}
+
+output "ec2_instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.app_server.id
+}
+
+output "ecr_repository_url" {
+  description = "URL of the ECR repository"
+  value       = aws_ecr_repository.app_repo.repository_url
+}
+
+output "ecr_repository_name" {
+  description = "Name of the ECR repository"
+  value       = aws_ecr_repository.app_repo.name
+}
+
+output "security_group_id" {
+  description = "ID of the security group"
+  value       = aws_security_group.node_app_sg.id
+}
